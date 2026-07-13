87511a587bd2fbb10650453322dc81f479ce204d
ohseyokr <ohseyokr@gmail.com>
2026-07-13 13:27:55 +0900

    Add files via upload

---
+++ public/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+// Render Cloud 대시보드의 Environment Variables가 자동으로 적용됩니다.
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  getClient: () => pool.connect()
+};
